60662076a9dd657046e90853ca6885923433370e
Aatmik-06 <adi120kush@gmail.com>
2024-10-05 18:20:23 +0530

    05 Oct

--- September/27 Sept React Router/React Router/src/App.jsx
+++ September/27 Sept React Router/React Router/src/App.jsx
@@ -525,22 +525,35 @@
 
 // React Forms
 
+// import { useEffect, useState } from "react";
+// const App=()=>{
+
+//   const [name,setName]=useState("")
+//   const [city,setCity]=useState("")
+//   return(
+//     <>
+//     <center>
+//     <h1>React Forms </h1>
+//     Enter Name : <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} /> <br />
+//     Enter City : <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}/> <br />
+//     <button>Submit</button>
+//     </center>
+//     </>
+//   )
+// }
+
+
+// 05 oct 
+
 import { useEffect, useState } from "react";
 const App=()=>{
-
-  const [name,setName]=useState("")
-  const [city,setCity]=useState("")
   return(
     <>
-    <center>
-    <h1>React Forms </h1>
-    Enter Name : <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} /> <br />
-    Enter City : <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}/> <br />
-    <button>Submit</button>
-    </center>
+   
     </>
   )
 }
+
 
 export default App;
 
